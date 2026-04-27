aae702dfeb8d01e28f1236edeedb0c0e316f2ecb
bolokfabio <bolok.fabio@iisgalvanimi.edu.it>
2026-04-27 09:10:59 +0000

    login con pandas

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import pandas as pd
 
 app = FastAPI()
+df = pd.read_excel("/workspaces/LOGIN-FASTAPI/dati.xlsx")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -16,3 +18,11 @@ def controlla(username: str = Form(...), password: str = Form(...)):
         return {"messaggio": 1}
     else:
         return {"messaggio": 0}
+
+@app.post("/login.Pandas")
+def controlla_password(username: str = Form(...), password: str = Form(...)):
+    risultato = df[(df["username"] == username)&(df["password"] == password)]
+    if not risultato.empty:
+        return {"messaggio":1}
+    else:
+        return {"messaggio":0}
--- static/script.js
+++ static/script.js
@@ -23,5 +23,32 @@ async function loggati() {
         document.getElementById("risultato").innerText = "Accesso negato";
     }
 }
+async function loggati_Pandas() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (!username || !password) {
+        alert("Inserisci username e password");
+        return;
+    }
+
+    const res = await fetch("/login.Pandas", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `username=${username}&password=${password}`
+    });
+
+    const dati = await res.json();
+
+    if (dati.messaggio == 1) {
+        document.getElementById("risultato").innerText = "Accesso effettuato";
+    } 
+    else {
+        document.getElementById("risultato").innerText = "Accesso negato";
+    }
+}
 
 document.getElementById('btn_login').addEventListener('click', loggati);
+document.getElementById('btn_login.Pandas').addEventListener('click', loggati_Pandas);
